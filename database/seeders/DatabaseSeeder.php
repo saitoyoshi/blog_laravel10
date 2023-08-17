ba57e5f7c3378539b19fee52c6524a4f223321ba
saitoyoshi <mitti180@gmail.com>
2023-08-17 11:28:28 +0900

    posts,tagsの中間テーブルに関連付けデータ挿入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(PostsTableSeeder::class);
+        $this->call([
+            PostsTableSeeder::class,
+            TagsTableSeeder::class,
+            PostTagTableSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
