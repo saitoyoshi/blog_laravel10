ef2744fe092a5c765a3a1e28f57779e70e53f56f
saitoyoshi <mitti180@gmail.com>
2023-08-18 16:39:22 +0900

    登録処理にトランザクションを実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ use App\Models\Tag;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class PostController extends Controller
@@ -22,31 +23,34 @@ class PostController extends Controller
     }
 
     public function store(PostFormRequest $request): RedirectResponse {
-        $post = new Post();
-        $post->user_id = Auth::user()->id;
-        $post->title = $request->title;
-        $post->content = $request->content;
+        return DB::transaction(function () use ($request) {
+            $post = new Post();
+            $post->user_id = Auth::user()->id;
+            $post->title = $request->title;
+            $post->content = $request->content;
 
-        $post->save();
-        // 半角と全角のスペースを半角スペースひとつに置換する
-        $tags = str_replace('　', ' ', trim($request->tags));
-        $tags = preg_replace('/\s+/', ' ', $tags);
-        $tags = explode(" ", $tags);
-        foreach ($tags as $tagname) {
-            // すでに同じ名前のタグが存在するかをチェック
-            $tag = Tag::where('name', $tagname)->first();
-            if (!$tag) {
-                $tag = new Tag();
-                $tag->name = $tagname;
-                $tag->save();
-            }
+            $post->save();
+            // 半角と全角のスペースを半角スペースひとつに置換する
+            $tags = str_replace('　', ' ', trim($request->tags));
+            $tags = preg_replace('/\s+/', ' ', $tags);
+            $tags = explode(" ", $tags);
+            foreach ($tags as $tagname) {
+                // すでに同じ名前のタグが存在するかをチェック
+                $tag = Tag::where('name', $tagname)->first();
+                if (!$tag) {
+                    $tag = new Tag();
+                    $tag->name = $tagname;
+                    $tag->save();
+                }
 
-            // タグが投稿に関連づけられていない場合のみ関連づける
-            if (!$post->tags->contains($tag->id)) {
-                $post->tags()->attach($tag->id);
+                // タグが投稿に関連づけられていない場合のみ関連づける
+                if (!$post->tags->contains($tag->id)) {
+                    $post->tags()->attach($tag->id);
+                }
             }
-        }
-        return redirect(route('post.index'))->with('message', $post->title . 'を投稿しました');
+            return redirect(route('post.index'))->with('message', $post->title . 'を投稿しました');
+
+        });
     }
 
     public function show(Post $post): View {
