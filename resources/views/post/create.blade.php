b1cfd81f21703f49ae01295479920905668958c1
saitoyoshi <mitti180@gmail.com>
2023-08-18 15:11:43 +0900

    oldヘルパー導入

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -17,15 +17,15 @@
     @csrf
     <div>
     <label for="">タイトル</label>
-    <input type="text" name="title">
+    <input type="text" name="title" value="{{ old('title') }}">
 </div>
 <div>
     <label for="">本文</label>
-    <textarea name="content" id="" cols="30" rows="10"></textarea>
+    <textarea name="content" id="" cols="30" rows="10">{{ old('content') }}</textarea>
 </div>
 <div>
     <label for="">タグ(複数の場合空白で区切ること)</label>
-    <input type="text" name="tags">
+    <input type="text" name="tags" value="{{ old('tags') }}">
 </div>
 <button>投稿</button>
   </form>
