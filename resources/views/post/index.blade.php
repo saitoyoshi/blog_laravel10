12cd0f65d8e3a82b42e41969fe6680eecaa4db7e
saitoyoshi <mitti180@gmail.com>
2023-08-20 06:17:54 +0900

    一覧画面にデザイン適応

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,25 +9,29 @@
         <p class="text-info">{{ session('message') }}</p>
     @endif
   @foreach($posts as $post)
-    <ul>
-        <li>タイトル：<a href="{{ route('post.show', $post) }}">{{ $post->title }}</a></li>
+  <div class="card border mb-3 mx-3">
+    <div class="card-body">
+        <h5 class="card-title"><a class="text-dark" href="{{ route('post.show', $post) }}">{{ $post->title }}</a></h5>
         @if (\illuminate\Support\Facades\Auth::id() === $post->user_id)
-        <a href="{{ route('post.edit', $post) }}"><button>更新</button></a>
-        <form action="{{ route('post.destroy', $post) }}" method="post">
+        <div class="">
+        <a href="{{ route('post.edit', $post) }}"><button class="btn btn-primary btn-sm">更新</button></a>
+        <form class="ms-2 d-inline" action="{{ route('post.destroy', $post) }}" method="post">
             @csrf
             @method('delete')
-            <button>削除</button>
+            <button class="btn btn-danger btn-sm">削除</button>
         </form>
+    </div>
         @else
-        <small>編集できません</small>
+        <small class="card-text text-info">編集できません</small>
         @endif
-        <li>Posted by {{ $post->user->name }} on {{ $post->updated_at->diffForHumans() }}</li>
-        <li>タグ:
+        <p class="card-text">Posted by {{ $post->user->name }} on {{ $post->updated_at->diffForHumans() }}</p>
+        <p class="card-text">タグ:
             @foreach ($post->tags as $tag)
                 <a href="/post?tagname={{ $tag->name }}">{{ $tag->name }}</a>
             @endforeach
-        </li>
-    </ul>
+        </p>
+    </div>
+</div>
   @endforeach
     <div>{{ $posts->links() }}</div>
 </x-layouts>
