6b6c4ec652f3cd0e7ec9ba8dea7dbc32f9087a83
saitoyoshi <mitti180@gmail.com>
2023-08-20 06:43:20 +0900

    本文の改行をフォーム入力のとおりに反映させる

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,10 +2,12 @@
     <x-slot:title>
         詳細画面
     </x-slot:title>
+    <div class="mb-4">
 <a href="{{ route('post.index') }}">戻る</a>
+</div>
     <ul>
         <li>タイトル：{{ $post->title }}</li>
-        <p>本文:{{ $post->content }}</p>
+        <p>本文:{!! nl2br(htmlspecialchars($post->content)) !!}</p>
         <li>Posted by {{ $post->user->name }} on {{ $post->updated_at->diffForHumans() }}</li>
         <li>タグ:
             @foreach ($post->tags as $tag)
