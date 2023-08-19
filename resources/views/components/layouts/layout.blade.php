e9e390a1874f572a6e361be205243ac533fc1220
saitoyoshi <mitti180@gmail.com>
2023-08-19 13:56:51 +0900

    botstrap dark modeをCDNで適応

--- resources/views/components/layouts/layout.blade.php
+++ resources/views/components/layouts/layout.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-dark-mode/1.0.2/bootstrap-dark-mode.min.css">
 </head>
 <body>
     <header>
