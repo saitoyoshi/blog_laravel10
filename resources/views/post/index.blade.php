1d45aa093457fc1fe9f2e5dc8591b0009b0fda58
saitoyoshi <mitti180@gmail.com>
2023-08-20 06:26:09 +0900

    一覧画面マージン調整

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,14 +4,14 @@
     </x-slot:title>
 
 @if(request()->has('tagname'))<a href="/post">投稿一覧</a>@endif
-    <a href="{{ route('post.create') }}">投稿する</a>
+    <div class="mb-4"><a href="{{ route('post.create') }}">投稿する</a></div>
     @if(session('message'))
         <p class="text-info">{{ session('message') }}</p>
     @endif
   @foreach($posts as $post)
-  <div class="card border mb-3 mx-3">
+  <div class="card border mb-3">
     <div class="card-body">
-        <h5 class="card-title"><a class="text-dark" href="{{ route('post.show', $post) }}">{{ $post->title }}</a></h5>
+        <h5 class="card-title"><a class="text-dark d-block" href="{{ route('post.show', $post) }}">{{ $post->title }}</a></h5>
         @if (\illuminate\Support\Facades\Auth::id() === $post->user_id)
         <div class="">
         <a href="{{ route('post.edit', $post) }}"><button class="btn btn-primary btn-sm">更新</button></a>
@@ -27,7 +27,7 @@
         <p class="card-text">Posted by {{ $post->user->name }} on {{ $post->updated_at->diffForHumans() }}</p>
         <p class="card-text">タグ:
             @foreach ($post->tags as $tag)
-                <a href="/post?tagname={{ $tag->name }}">{{ $tag->name }}</a>
+                <a href="/post?tagname={{ $tag->name }}">{{ $tag->name }}&nbsp;&nbsp;</a>
             @endforeach
         </p>
     </div>
